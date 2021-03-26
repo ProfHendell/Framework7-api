336f4a7618213879cc92ab0f12412510866f0365
MrHendell <hendelporras@hotmail.com>
2021-03-26 09:53:58 -0600

    Tabla usuarios

--- src/sql/tables.sql
+++ src/sql/tables.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE IF NOT EXISTS `framework7_api`;
+CREATE DATABASE IF NOT EXISTS `asistencia`;
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+	`id_usuario` INT NOT NULL AUTO_INCREMENT,
+    `correo` VARCHAR(150) NOT NULL,
+    `password` VARCHAR(250) NOT NULL,
+    PRIMARY KEY (`id_usuario`)
+);
